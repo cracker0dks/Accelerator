71c231554f2fafb1575a4f3a234b6f5ca66c5caa
luroc <luroc@posteo.de>
2020-04-01 01:00:03 +0200

    move camera button to buttom bar
    Move button to start own camera stream to the bottom bar to have it next
    to the start microphone stream button.

--- public/js/uiEvents.js
+++ public/js/uiEvents.js
@@ -1137,6 +1137,8 @@ $(function () { //Document ready
 				status = "not-coffee";
 			} else if ($(this).find(".applause").length > 0) {
 				status = "not-applause";
+			} else if ($(this).find(".fa-video-camera").length > 0) {
+				stopLocalVideo();
 			}
 		} else {
 			$(this).addClass("alert-danger");
@@ -1166,6 +1168,11 @@ $(function () { //Document ready
 				status = "coffee";
 			} else if ($(this).find(".applause").length > 0) {
 				status = "applause";
+			} else if ($(this).find(".fa-video-camera").length > 0) {
+				if ($(this).hasClass("alert-error")) {
+					$(this).removeClass("alert-error");
+				}
+				startLocalVideo();
 			}
 		}
 		if (status != "")
